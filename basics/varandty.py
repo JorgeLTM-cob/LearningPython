82b0b9eb928965118c09e1fdfa480066f3452eb6
JorgeLTM-cob <77084960+JorgeLTM-cob@users.noreply.github.com>
2021-04-29 14:37:30 -0500

    Add files via upload

---
+++ basics/varandty.py
@@ -0,0 +1,21 @@
+#Integer definition
+myint = 7
+#Float definition
+myfloat = 7.0
+mydecimal = float(7)
+#Strings
+Mystring = 'Hola'
+MyString = "Hola"
+Mysampstr = "No te preocupes de los 'apostrofes' usando comillas dobles"
+
+#Operadores
+
+uno = 1
+dos = 2
+tres = uno + dos
+
+hola = "hola"
+mundo = "mundo"
+holamundo = hola + " " + mundo
+a, b = 3, 4
+
---
+++ basics/varandty_2.py
@@ -0,0 +1,9 @@
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+if mystring == "hello":
+   print ("Cadena: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+   print ("Flotante: %d" % myfloat)
+if isinstance(myint, int) and myint == 20:
+   print ("Integral: %d" % myint)
