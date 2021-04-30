452538e4c9ebb888667454ca5135718d3075e622
JorgeLTM-cob <77084960+JorgeLTM-cob@users.noreply.github.com>
2021-04-30 14:10:54 -0500

    Add files via upload

---
+++ basics/exlists.py
@@ -0,0 +1,12 @@
+# modificando el codigo
+numeros = []
+cadenas = []
+numeros.append(1)
+numeros.append(2)
+numeros.append(3)
+cadenas.append("hola")
+cadenas.append("mundo")
+
+# probando codigo
+print (numeros)
+print (cadenas)
