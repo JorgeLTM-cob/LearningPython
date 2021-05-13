11cc608b5c9ea797c1b4461bae49249cebd966d7
JorgeLTM-cob <77084960+JorgeLTM-cob@users.noreply.github.com>
2021-05-12 23:15:49 -0500

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,33 @@
+#Python follows hierarchy of operations
+number = 1 + 2 * 3 / 4.0
+print (number)
+
+#The following operation returns the remainder of the division
+
+remainder = 11 % 3
+print (remainder)
+
+#Using two multiplication symbols makes a power relationship
+squared = 7**2
+cubed = 2**3
+print (squared)
+print (cubed)
+
+#Concatenating strings
+
+helloworld = "hello" + " " + "world"
+print (helloworld)
+
+#Multiplying strings yields to a repeated string
+lotsofhellos = "hello" * 10
+print (lotsofhellos)
+
+#Lists can be joined with the addition operators:
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+#Python forms new lists when you multiply a list by another number
+print([1,2,3] * 3)
+
