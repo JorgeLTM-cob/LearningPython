dbfc8fe8f083d7b11b00d223de57b95f4ee66eae
JorgeLTM-cob <77084960+JorgeLTM-cob@users.noreply.github.com>
2021-05-14 23:52:15 -0500

    Add files via upload

---
+++ basics/strings.py
@@ -0,0 +1,17 @@
+name = "John"
+print("Hello, %s" % name)
+
+name2 = "John"
+age = 23
+print("%s is %d years old." % (name,age))
+
+#Any object which is not a string can be formatted using the %s operator as well
+
+mylist = [1,2,3]
+print("A list: %s" % mylist)
+
+# %s string
+# %d integer
+# %f floating point numbers
+# %.<number of digits>f
+# %x/%X Integers in hex representation
