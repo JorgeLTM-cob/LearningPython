a1eff9ef017d26fb383e56de36dbe164e2d0bf29
JorgeLTM-cob <77084960+JorgeLTM-cob@users.noreply.github.com>
2021-05-30 01:23:23 -0500

    Add files via upload

---
+++ basics/functions.py
@@ -0,0 +1,7 @@
+def my_function():
+    print ("Hello From My Function!")
+
+def my_function_with_args(username, greeting):
+    print("Hello, %s , From My Function! I wish you %s" % username)
+
+my_function()
